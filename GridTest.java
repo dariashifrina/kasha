57c0372269ab1e923ba43ddadf61e429fd5a6c93
Karina Ionkina <kionkina@stuy.edu>
2017-01-11 16:51:36 -0500

    possible grid?

---
+++ GridTest.java
@@ -0,0 +1,8 @@
+public class GridTest{
+    public static void main(String[] args){
+        System.out.println(" __ __ __ __ __ __ __");
+	for (int i=0; i < 6; i++){
+	    System.out.println("|__|__|__|__|__|__|__|");
+	}
+    }
+}
