f60d2cb1bbb1dcdeee5eea19f5a4f3f5d04f965b
Karina Ionkina <Karina@Karinas-MacBook-Air.local>
2017-01-12 20:08:51 -0500

    Passanger class created, boxes in the grid now contain passenger objects, whos toString prints %

--- GridTest.java
+++ GridTest.java
@@ -1,11 +1,7 @@
-import java.util.ArrayList;
-import java.util.*;
-import java.io.*;
 
 public class GridTest{
-    public static String colorStart = "\u001B[31m";
-    public static String colorEnd = "\u001B[0m_";
-  public static void populate(String[][] a){
+
+  public static void populate(Object[][] a){
 	String returnString = "";
 	int cols = a.length;
 	int rows = a[0].length;
@@ -21,9 +17,13 @@ public class GridTest{
 	}
   }
     
-    public static void printb(String[][] b){
+    public static void printb(Object[][] b){
 	int cols = b.length;
 	int rows = b[0].length;
+       	for(int top =0; top < cols; top ++){
+	    System.out.print ( " ___");
+	}
+	System.out.print("\n");
 	for(int c=0; c < cols; c++){
 	    for(int r=0; r < rows; r++){
 		System.out.print(b[c][r]);
@@ -32,20 +32,23 @@ public class GridTest{
 	}
     }
 
-    public static void addPerson(String[][] a, int xcor, int ycor){
-	a[xcor][ycor] = "|_" + colorStart + "%" + colorEnd;
+    public static void addPerson(Object[][] a, int xcor, int ycor){
+	Passenger bob = new Passenger();
+	bob.setCords(xcor, ycor);
+	a[xcor][ycor] = bob;
     }
-   
-    
+
+  
 		
     public static void main(String[] args){
-	String[][] board = new String[10][10];
+	Object[][] board = new Object[10][10];
 	populate(board);
 	addPerson(board, 4, 2);
 	addPerson(board, 5, 9);
 	addPerson(board, 1, 1);
 	addPerson(board, 9, 1);
         printb(board);
+	
     }
 }
 
