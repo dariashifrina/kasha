3e714c86a95416086b66c08faa3ea282d696b40b
Daria Shifrina <Daria Shifrina>
2017-01-13 20:25:09 -0500

    created references file and terminal

---
+++ Terminal.java
@@ -0,0 +1,16 @@
+public class Terminal{
+	    //sets color of passenger toString to red
+    public static String colorStart = "\u001B[31m";
+    public static String colorEnd = "\u001B[0m";
+    private int xcor;
+    private int ycor;
+    
+    public String toString(){
+	return "|_" + colorStart + "$" + colorEnd + "_";
+    }
+
+    public void setCords(int x, int y){
+	xcor = x;
+	ycor = y;
+    }
+}
